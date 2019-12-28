499735357a41463075aab2ffa1376c132da38e8b
avasilistov <avasilistov@gmail.com>
2019-12-28 17:05:58 +0200

    Simplest Python solution "Take a Ten Minute Walk" with Unittest

--- take_a_ten_minute_walk.py
+++ take_a_ten_minute_walk.py
@@ -1,16 +1,2 @@
-def is_valid_walk(walk):
-    if len(walk) != 10:
-        return False
-    dict_direction = {}
-    for direction in  walk:
-        dict_direction[direction] = walk.count(direction)
-    if 'n' in dict_direction.keys() and dict_direction.get('n') != dict_direction.get('s'):
-        return False
-    if 'w' in dict_direction.keys() and dict_direction.get('w') != dict_direction.get('e'):
-        return False
-    if 's' in dict_direction.keys() and dict_direction.get('n') != dict_direction.get('s'):
-        return False
-    if 'e' in dict_direction.keys() and dict_direction.get('w') != dict_direction.get('e'):
-        return False
-
-    return True
+def isValidWalk(walk):
+    return len(walk) == 10 and walk.count('n') == walk.count('s') and walk.count('e') == walk.count('w')
